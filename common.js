bcb841b33bd7ccf46dbfe796333a4ded82eb35b9
Alex Frazer <alexadfrazer@gmail.com>
2015-03-05 09:10:53 -0500

    basic flow working and creating a service in the database, but not properly authenticating yet. Getting error with the state

--- common.js
+++
@@ -1,7 +0,0 @@
-if (!Accounts.battlenet) {
-	Accounts.battlenet = {};
-}
-
-Accounts.battlenet.config = function(options) {
-	Accounts.battlenet._options = options;
-};
--- package.js
+++ package.js
@@ -1,18 +1,24 @@
 Package.describe({
-  summary: "Login service for Battlenet accounts"
+  summary: "Login service for battlenet accounts"
 });
 
-Package.on_use(function(api) {
+Package.onUse(function(api) {
   api.use('accounts-base', ['client', 'server']);
-  api.use('accounts-oauth2', ['client', 'server']);
-  api.use('http', ['client', 'server']);
-  api.use('templating', 'client');
+  api.imply('accounts-base', ['client', 'server']);
+  api.use('accounts-oauth', ['client', 'server']);
+  api.use('oauth2@1.1.2', ['client', 'server']);
+  api.use('oauth@1.1.3', ['client', 'server']);
+  api.use('http@1.0.10', ['client', 'server']);
+  api.use(['underscore@1.0.2', 'service-configuration@1.0.3'], ['client', 'server']);
+  api.use(['random@1.0.2', 'templating@1.0.11'], 'client');
 
-  api.add_files(
-    ['configure.html', 'configure.js'],
-    'client');
+  api.export('battlenet');
 
-  api.add_files('common.js', ['client', 'server']);
-  api.add_files('server.js', 'server');
-  api.add_files('client.js', 'client');
+	api.add_files(
+  ['lib/battlenet_configure.html', 'lib/battlenet_configure.js'],
+  'client');
+
+  api.add_files('lib/battlenet_account.js');
+  api.add_files('lib/battlenet_client.js', 'client');
+  api.add_files('lib/battlenet_server.js', 'server');
 });
